a9d186c2702cb4ad21d51bfe607763d4e25933fe
marwan <marwan.h1994@gmail.com>
2023-06-04 23:22:23 +0200

    added condition if the cityname is not submitted

--- homework/HackYourTemperature/server.js
+++ homework/HackYourTemperature/server.js
@@ -1,11 +1,21 @@
 import express from "express"
-import routes from "./routes/router.js"
 
 const app = express()
 const PORT = 3001
-
 app.use(express.json())
-app.use("/", routes)
+
+app.get("/", (req, res) => {
+    res.send("Hello from the frontend")
+})
+
+app.post("/weather", (req, res) => {
+    const cityName = req.body.cityName
+    if (!cityName) {
+        res.status(400).send("City name is required")
+    } else {
+        res.send(`You submitted: ${cityName}`)
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`The server runs at Port ${PORT}`)
